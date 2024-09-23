bee08c28edafb06692ce6182bb38f0fdc92ffc8c
KI0629 <qscft9898@naver.com>
2024-09-23 16:31:09 +0900

    add, modfiy : 회원, 1:1문의 엔티티, 레포지토리 생성

--- src/main/java/com/cinema/repository/AnswerRepository.java
+++ src/main/java/com/cinema/repository/AnswerRepository.java
@@ -1,5 +1,6 @@
 package com.cinema.repository;
 
+import com.cinema.domain.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
--- src/main/java/com/cinema/repository/MovieRepository.java
+++ src/main/java/com/cinema/repository/MovieRepository.java
@@ -1,6 +1,7 @@
 package com.cinema.repository;
 
+import com.cinema.domain.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MovieRepository extends JpaRepository<Movie, long> {
+public interface MovieRepository extends JpaRepository<Movie, Long> {
 }
