880a5fc9dc41dfbc6404bd4f50dd7e6874bcddb8
cfgop23 <cfgop23@gmail.com>
2024-09-30 16:49:51 +0900

    modify, feat : Reservation 엔터티 수정 및 ReservationDto 생성

--- src/main/java/com/cinema/domain/Reservation.java
+++ src/main/java/com/cinema/domain/Reservation.java
@@ -1,68 +1,72 @@
 package com.cinema.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
-import java.util.Date;
+import java.time.LocalDateTime;
+import lombok.*;
 
 @Entity
 @Table(name = "movie_reservation_tbl")
 @Getter
+@ToString
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Reservation {
-    @Id
-    private String rNum;            // 영화예매번호
+  @Id
+  @Size(min = 12)
+  private String rNum; // 영화예매번호
 
-    @Column(nullable = false, length = 3)
-    private String seatNum;         // 영화예매 좌석번호
+  @Column(nullable = false, length = 3)
+  private String seatNum; // 영화예매 좌석번호
 
-    @Column(nullable = false)
-    private int paymentAmount;      // 영화예매 결제금액
+  @Column(nullable = false)
+  private int paymentAmount; // 영화예매 결제금액
 
-    @Column(nullable = false)
-    private Date rDate;             // 영화예매일자
+  @Column(nullable = false)
+  private LocalDateTime rDate; // 영화예매일자
 
-    @Column(length = 1)
-    private int isCanceled;         // 영화예매 취소여부
+  @Column(nullable = true, length = 1)
+  private Integer isCanceled; // 영화예매 취소여부
 
-    private Date cancelDate;        // 영화예매 취소일자
+  @Column(nullable = true)
+  private LocalDateTime cancelDate; // 영화예매 취소일자
 
-    @Column(length = 500)
-    private String reviewContent;   // 관람후기내용
+  @Column(nullable = true, length = 500)
+  private String reviewContent; // 관람후기내용
 
-    @Column(precision = 3, scale = 1)
-    private BigDecimal reviewRating;       // 관람후기평점
+  @Column(precision = 3, scale = 1)
+  private BigDecimal reviewRating; // 관람후기평점
 
-    @Column(length = 3, nullable = false)
-    private String rPersonType1;    // 예매인원1 구분
+  @Column(length = 3, nullable = false)
+  private String rPersonType1; // 예매인원1 구분
 
-    @Column(length = 3)
-    private String rPersonType2;    // 예매인원2 구분
+  @Column(length = 3)
+  private String rPersonType2; // 예매인원2 구분
 
-    @Column(length = 3)
-    private String rPersonType3;    // 예매인원3 구분
+  @Column(length = 3)
+  private String rPersonType3; // 예매인원3 구분
 
-    @Column(length = 3)
-    private String rPersonType4;    // 예매인원4 구분
+  @Column(length = 3)
+  private String rPersonType4; // 예매인원4 구분
 
-    @Column(length = 3)
-    private String rPersonType5;    // 예매인원5 구분
+  @Column(length = 3)
+  private String rPersonType5; // 예매인원5 구분
 
-    @Column(length = 3)
-    private String rPersonType6;    // 예매인원6 구분
+  @Column(length = 3)
+  private String rPersonType6; // 예매인원6 구분
 
-    @Column(nullable = false)
-    private int rPersonPrice1;      // 예매인원1 가격
-    private int rPersonPrice2;      // 예매인원2 가격
-    private int rPersonPrice3;      // 예매인원3 가격
-    private int rPersonPrice4;      // 예매인원4 가격
-    private int rPersonPrice5;      // 예매인원5 가격
-    private int rPersonPrice6;      // 예매인원6 가격
+  @Column(nullable = false)
+  private int rPersonPrice1; // 예매인원1 가격
+
+  private int rPersonPrice2; // 예매인원2 가격
+  private int rPersonPrice3; // 예매인원3 가격
+  private int rPersonPrice4; // 예매인원4 가격
+  private int rPersonPrice5; // 예매인원5 가격
+  private int rPersonPrice6; // 예매인원6 가격
+
+  @ManyToOne
+  @JoinColumn(name = "movie_num") // fk 역할
+  private Movie movie;
 }
--- src/main/java/com/cinema/dto/reservation/ReservationDTO.java
+++ src/main/java/com/cinema/dto/reservation/ReservationDTO.java
@@ -1,4 +1,42 @@
 package com.cinema.dto.reservation;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReservationDTO {
+  private String rNum; // 영화예매번호
+  private String seatNum; // 영화예매 좌석번호
+  private int paymentAmount; // 영화예매 결제금액
+
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm:ss")
+  private LocalDateTime rDate; // 영화예매일자
+
+  private int isCanceled; // 영화예매 취소여부
+
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm:ss")
+  private LocalDateTime cancelDate; // 영화예매 취소일자
+
+  private String reviewContent; // 관람후기내용
+  private BigDecimal reviewRating; // 관람후기평점
+  private String rPersonType1; // 예매인원1 구분
+  private String rPersonType2; // 예매인원2 구분
+  private String rPersonType3; // 예매인원3 구분
+  private String rPersonType4; // 예매인원4 구분
+  private String rPersonType5; // 예매인원5 구분
+  private String rPersonType6; // 예매인원6 구분
+  private int rPersonPrice1; // 예매인원1 가격
+  private int rPersonPrice2; // 예매인원2 가격
+  private int rPersonPrice3; // 예매인원3 가격
+  private int rPersonPrice4; // 예매인원4 가격
+  private int rPersonPrice5; // 예매인원5 가격
+  private int rPersonPrice6; // 예매인원6 가격
 }
