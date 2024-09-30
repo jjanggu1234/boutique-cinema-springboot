6edc937cd3a47b88df9b51e5996768296799daa7
rkdlqdhksfy <skfkrk4321@gamil.com>
2024-09-30 16:51:15 +0900

    modify : throw Exception 예외처리 추가(#35)

--- src/main/java/com/cinema/service/MovieService.java
+++ src/main/java/com/cinema/service/MovieService.java
@@ -5,13 +5,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface MovieService {
-    Long register (MovieDTO movieDTO);    // 등록
-    MovieDTO get(Long movieNum);          // 상세 조회
-    void modify(MovieDTO movieDTO);       // 수정
-    void remove(Long movieNum);           // 삭제
-    Page<MovieDTO> getfindAll(Pageable pageable); // 목록 조회 (페이징) (전체 목록 조회)
-    Page<MovieDTO> findByKorTitle(String korTitle, Pageable pageable);  // 목록 조회 (제목 검색)
-    Page<MovieDTO> getMoviesLatestByDate(Pageable pageable); // 최신순으로 정렬된 목록 조회
-    Page<MovieDTO> getMoviesEarliestByDate(Pageable pageable); // 오래된순으로 정렬된 목록 조회
-    Page<MovieDTO> getMoviesByTheaterNum(Integer theaterNum, Pageable pageable); // 상영관 번호로 조회
+    Long register (MovieDTO movieDTO) throws Exception;    // 등록
+    MovieDTO get(Long movieNum) throws Exception;          // 상세 조회
+    void modify(MovieDTO movieDTO) throws Exception;     // 수정
+    void remove(Long movieNum) throws Exception;             // 삭제
+    Page<MovieDTO> getfindAll(Pageable pageable) throws Exception;   // 목록 조회 (페이징) (전체 목록 조회)
+    Page<MovieDTO> findByKorTitle(String korTitle, Pageable pageable) throws Exception;  // 목록 조회 (제목 검색)
+    Page<MovieDTO> getMoviesLatestByDate(Pageable pageable) throws Exception;   // 최신순으로 정렬된 목록 조회
+    Page<MovieDTO> getMoviesEarliestByDate(Pageable pageable) throws Exception;   // 오래된순으로 정렬된 목록 조회
+    Page<MovieDTO> getMoviesByTheaterNum(Integer theaterNum, Pageable pageable) throws Exception;  // 상영관 번호로 조회
 }
