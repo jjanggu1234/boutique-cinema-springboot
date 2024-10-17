537c2e748e41aa00d5f0b12d5d1a7f1d16cc6274
jinwoo1021 <wkdwlstjr02@kyonggi.ac.kr>
2024-10-17 16:08:16 +0900

    modify : 공지사항 기능수정 완료!!(#66)

--- src/main/java/com/cinema/repository/NoticeRepository.java
+++ src/main/java/com/cinema/repository/NoticeRepository.java
@@ -1,8 +1,20 @@
 package com.cinema.repository;
 
 import com.cinema.domain.Notice;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface NoticeRepository  extends JpaRepository<Notice, Long> {
 
+
+     @Query("SELECT n FROM Notice n WHERE n.nTitle LIKE ?1")
+     Page<Notice> findByNTitle(String title, Pageable pageable);
+
+     @Query("SELECT n FROM Notice n ORDER BY n.nDate DESC")
+     Page<Notice> findLatestByDate(Pageable pageable);
+
+      @Query("SELECT n FROM Notice n ORDER BY n.nDate ASC")
+      Page<Notice> findEarliestByDate(Pageable pageable);
 }
