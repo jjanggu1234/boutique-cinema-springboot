58a91b012a50ccb38b38496d50addfddbf879feb
cfgop23 <cfgop23@gmail.com>
2024-10-24 11:15:04 +0900

    modify : 예매 컨트롤러 로그인권한 설정

--- src/main/java/com/cinema/controller/ReservationController.java
+++ src/main/java/com/cinema/controller/ReservationController.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -22,6 +23,7 @@ public class ReservationController {
   private final ReservationRepository reservationRepository;
   private final ModelMapper modelMapper;
 
+  @PreAuthorize("hasAnyRole('ROLE_USER')")
   @PostMapping
   public ResponseEntity<ReservationDTO> createReservation(@Valid ReservationDTO reservationDTO)
       throws Exception {
@@ -31,12 +33,14 @@ public class ReservationController {
     return new ResponseEntity<>(createdReservationDTO, HttpStatus.CREATED);
   }
 
+  @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
   @GetMapping("/list")
   public ResponseEntity<List<ReservationDTO>> getAllReservations() {
     List<ReservationDTO> reservations = reservationService.getAllReservations();
     return ResponseEntity.ok(reservations);
   }
 
+  @PreAuthorize("hasAnyRole('ROLE_USER')")
   @GetMapping("/member/{mId}")
   public ResponseEntity<List<ReservationDTO>> getReservationsByMember(
       @PathVariable("mId") String mId) throws Exception {
@@ -45,6 +49,7 @@ public class ReservationController {
     return ResponseEntity.ok(reservations);
   }
 
+  @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
   @GetMapping("/{rNum}")
   public ResponseEntity<ReservationDTO> getReservationByRNum(@PathVariable String rNum)
       throws Exception {
@@ -53,6 +58,7 @@ public class ReservationController {
     return ResponseEntity.ok(reservationDTO);
   }
 
+  @PreAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
   @PutMapping("/cancel/{rNum}")
   public ResponseEntity<String> cancelReservation(@PathVariable String rNum) throws Exception {
     reservationService.cancelReservation(rNum);
@@ -60,6 +66,7 @@ public class ReservationController {
     return ResponseEntity.ok("Reservation canceled successfully.");
   }
 
+  @PreAuthorize("hasAnyRole('ROLE_USER')")
   @PutMapping("/review/{rNum}")
   public ResponseEntity<String> updateReview(
       @PathVariable String rNum, @RequestBody @Valid ReviewDTO reviewDTO) throws Exception {
